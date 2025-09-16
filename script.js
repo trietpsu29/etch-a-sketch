beaad92077de9a2de79cd90ff6d68530644ec50c
trietpsu29 <112594221+trietpsu29@users.noreply.github.com>
2025-09-16 20:18:58 +0700

    Implement EventListener on grid
    With event lisnter, player can hover the grid to
    draw.

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ for (let i = 1; i <= 256; i++) {
   square.textContent = "";
   container.appendChild(square);
 }
+const square = document.querySelector("div");
+square.addEventListener("mousemove", function (e) {
+  e.target.style.background = "lightblue";
+});
