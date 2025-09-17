00a2d5206e65c6988b5a299f82e85d1219636f1a
trietpsu29 <112594221+trietpsu29@users.noreply.github.com>
2025-09-17 18:06:08 +0700

    Implement change size button
    With this button, player can change size of the grid between 1 and 100.
    The grid's size will be changed dynamically based on player's option.

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const container = document.querySelector(".container");
-for (let i = 1; i <= 256; i++) {
-  let square = document.createElement("div");
-  square.className = "square";
-  square.textContent = "";
-  container.appendChild(square);
+
+function makeGrid(n) {
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+  const canvasWidth = container.offsetWidth;
+  for (let i = 1; i <= n * n; i++) {
+    let square = document.createElement("div");
+    square.className = "square";
+    square.textContent = "";
+    square.style.width = `${canvasWidth / n}px`;
+    square.style.height = `${canvasWidth / n}px`;
+    container.appendChild(square);
+  }
 }
-const square = document.querySelector("div");
+
+makeGrid(16); //default size
+
+const square = document.querySelector(".container");
 square.addEventListener("mousemove", function (e) {
   e.target.style.background = "lightblue";
+});
+const sizeBtn = document.querySelector(".size");
+sizeBtn.addEventListener("click", function (e) {
+  let playerSize = prompt("Choose canvas's size between 1 and 100");
+  while (playerSize < 1 || playerSize > 100) {
+    playerSize = prompt("Size is out of range, please choose again!");
+  }
+  makeGrid(playerSize);
 });
